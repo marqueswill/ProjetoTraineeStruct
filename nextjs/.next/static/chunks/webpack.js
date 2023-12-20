51ddc19d53a88b9aee0ab524c93a322ee6cacf8d
Willyan Marques <willyanmarquesmelo@gmail.com>
2023-12-20 15:29:39 -0300

    proteção pagina edicao

--- nextjs/.next/static/chunks/webpack.js
+++ nextjs/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a1c469cbf4498c88"; }
+/******/ 		__webpack_require__.h = function() { return "65387db236c898ab"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
