893bc8794d2afce5be22b0acff1d644362ab1903
Willyan Marques <willyanmarquesmelo@gmail.com>
2023-12-20 19:38:23 -0300

    ajustes

--- nextjs/.next/static/chunks/webpack.js
+++ nextjs/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "530b74a7bceeda58"; }
+/******/ 		__webpack_require__.h = function() { return "3f08ee635ee02c77"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- nextjs/src/pages/easteregg.tsx
+++ nextjs/src/pages/easteregg.tsx
@@ -1,8 +1,11 @@
 import styles from "@/styles/App.module.css";
 export default function Teste() {
   return (
+    // <div className={styles.teste}>
+    //   <img src="/images/background/teste.gif" alt="" />
+    // </div>
     <div className={styles.teste}>
-      <img src="/images/background/teste.gif" alt="" />
+      <img src="/images/background/teste2.gif" alt="" />
     </div>
   );
 }
